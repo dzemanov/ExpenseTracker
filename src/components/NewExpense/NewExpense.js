32748c64bfdf69a468de4b2d76c1be1a8f06af86
xzemanov <zemanovicova.dominika@gmail.com>
2022-08-23 11:48:41 +0200

    hide input form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,39 @@
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditingNew, setIsEditingNew] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString() // not optimal
+      id: Math.random().toString(), // not optimal
     };
     props.onAddExpense(expenseData);
+    setIsEditingNew(false);
+  };
+
+  const cancelEditingHandler = () => {
+    setIsEditingNew(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditingNew(true);
   };
 
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {isEditingNew ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelEditingHandler}
+        />
+      ) : (
+        <button type="button" onClick={startEditingHandler}>
+          Add New Expense
+        </button>
+      )}
     </div>
   );
 };
